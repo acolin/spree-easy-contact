ab7c2db1c65bab8a5f3124d0713366245303adfc
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-24 17:29:00 -0500

    added last name, first name, phone fields

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   
-  validates :name, :presence => true
+  validates_presence_of :first_name, :last_name, :phone
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :order_number, :format => {:with => /(^$)|(^R\d{9}$)/i, :message => I18n.t("invalid_order_number")}
   
--- db/migrate/20110203113622_create_contact_table.rb
+++ db/migrate/20110203113622_create_contact_table.rb
@@ -1,8 +1,10 @@
 class CreateContactTable < ActiveRecord::Migration
   def self.up
     create_table :contacts do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :email
+      t.string :phone
       t.string :order_number
       t.text :message
       t.references :topic
