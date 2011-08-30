cc292f72c9d2cba718dff35ce3a8e4a7f16c37a3
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-30 17:13:48 -0500

    quick push

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@
 $('#contact-us').click(function(event) {
 	$.get("/contacts/new.js", function(data) {
 		// Show Spin while contact form is loading
-		// $('#contact-us-modal').spin();
+		$('#contact-us-modal').spin();
 
 		// When image finishes loading, remove the spinner and show contact form
 		$('#contact-us-modal').load(function (){
