590ffa84d093433efc2cf7920dcf3747289d4ef2
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-30 19:01:59 -0500

    quick push

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,15 @@
 //Loads the contact us form via an ajax request from a link with attribute load_contact_us=path_to_contact_us_form
 
 $('#contact-us').click(function(event) {
-	$.get("/contacts/new", function(data) {
-		// Show Spin while contact form is loading
-		$("#contact-us-modal").spin();
 
+	// Show Spin while contact form is loading
+	$("#contact-us-modal").spin();
+	
+	//Load via ajax the contact form in html
+	$.get("/contacts/new", function(data) {
 		// When image finishes loading, remove the spinner and show contact form
-		$('#contact-us-modal').load(function (data){
 			$(this).empty();	
 			$(this).append(data);
 			$(this).fadeIn("slow");
-		});
 	});
 });
