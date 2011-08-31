7dc356515e48638328aec843237fec44de07526f
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-31 12:38:41 -0500

    quick push

--- public/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//Loads the contact us form via an ajax request
-// $(document).ready(function() {	
-	//Load via ajax the contact form in html
-	$.get("/contacts/new", function(data) {
-			$('#contact-us-modal').append(data);
-	});
-// });
