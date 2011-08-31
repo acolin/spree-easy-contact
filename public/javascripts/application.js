dd6ed4b910f54feb5b21860d94a457898b9cf454
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-30 19:33:26 -0500

    quick push

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,5 @@
 //Loads the contact us form via an ajax request from a link with attribute load_contact_us=path_to_contact_us_form
-
-$('#contact-us').click(function(event) {
-
-	// Show Spin while contact form is loading
-	$("#contact-us-modal").spin();
-	
+$(document).ready(function() {	
 	//Load via ajax the contact form in html
 	$.get("/contacts/new", function(data) {
 		// When image finishes loading, remove the spinner and show contact form
