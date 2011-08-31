fced5344eb0b42ada7886f3d49ae4096527d91cd
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-31 12:21:41 -0500

    quick push

--- public/javascripts/spree-easy-contact.js
+++ public/javascripts/spree-easy-contact.js
@@ -1,3 +1,7 @@
-$('#contact-us').click(function () {
-	$.get
-})
+//Loads the contact us form via an ajax request
+// $(document).ready(function() {	
+	//Load via ajax the contact form in html
+	$.get("/contacts/new", function(data) {
+			$('#contact-us-modal').append(data);
+	});
+// });
