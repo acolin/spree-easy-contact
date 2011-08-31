d2a0dfc30366056ff9b360d1074a4859b834f1d2
Arnoldo Rodriguez <acolin@incaztech.com>
2011-08-31 16:28:58 -0500

    quick push

--- lib/spree_easy_contact_hooks.rb
+++ lib/spree_easy_contact_hooks.rb
@@ -4,13 +4,9 @@ class SpreeEasyContactHooks < Spree::ThemeSupport::HookListener
     %(<%= tab(:contacts) %>)
   end
   
-  insert_after :inside_head do
-    'contacts/content_for_head'
-  end
+  insert_after :inside_head, :partial => 'contacts/content_for_head'
   
-  insert_after :inside_head do
-    'contacts/contact_us_holder'
-  end
+  insert_after :inside_head, :partial => 'contacts/contact_us_holder'
   
   insert_after :admin_configurations_menu do
     "<%= configurations_menu_item(I18n.t('topics'), admin_topics_path, I18n.t('topics_description')) %>"
