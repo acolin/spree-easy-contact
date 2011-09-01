dd49dda0bcab397bb126863209527bd1e57a353a
Arnoldo Rodriguez <acolin@incaztech.com>
2011-09-01 11:33:10 -0500

    quick push

--- db/migrate/20110831235658_add_is_read_to_contacts.rb
+++
@@ -1,9 +0,0 @@
-class AddIsReadToContacts < ActiveRecord::Migration
-  def self.up
-    add_column :contacts, :is_read, :boolean
-  end
-
-  def self.down
-    remove_column :contacts, :is_read
-  end
-end
