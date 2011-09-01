b60aae43d9b408ad8be125573cbeda9c0298c7cf
Arnoldo Rodriguez <acolin@incaztech.com>
2011-09-01 12:07:43 -0500

    quick push

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
     
-  resources :contacts, :controller => 'contacts' do
-    get 'mark_as_read', :on => :member
-  end
-  
+  resources :contacts, :controller => 'contacts'
+    
   namespace :admin do
-    resources :contacts
+    resources :contacts do
+      get 'mark_as_read', :on => :member
+    end
     resources :topics
   end
 end
