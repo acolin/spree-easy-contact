78e36c4ef33c1e9b951884166277ee71607ca0e1
Arnoldo Rodriguez Colin <acolin@incaztech.com>
2011-09-21 11:52:53 -0500

    added extrtra hook for contact us option in hooks file insert after inside_secondary_nav

--- lib/spree_easy_contact_hooks.rb
+++ lib/spree_easy_contact_hooks.rb
@@ -6,10 +6,14 @@ class SpreeEasyContactHooks < Spree::ThemeSupport::HookListener
   
   insert_after :inside_head, :partial => 'contacts/content_for_head'
   
-  insert_after :inside_head, :partial => 'contacts/contact_us_holder'
+  insert_before :inside_footer, :partial => 'contacts/contact_us_holder'
   
   insert_after :admin_configurations_menu do
     "<%= configurations_menu_item(I18n.t('topics'), admin_topics_path, I18n.t('topics_description')) %>"
+  end
+  
+  insert_after :inside_secondary_nav do
+    '<li><a href="#" id="contact-us" data-reveal-id="contact-us-modal"><%= t("contact_us") %></a></li>'
   end
   
 end
