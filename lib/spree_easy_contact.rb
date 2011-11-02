230595d83c9239d1caff3eacd6dc35eafd533370
Arnoldo Rodriguez Colin <acolin@incaztech.com>
2011-11-02 12:47:23 -0600

    made optional phone and required message

--- lib/spree_easy_contact.rb
+++ lib/spree_easy_contact.rb
@@ -16,3 +16,28 @@ module SpreeEasyContact
     config.to_prepare &method(:activate).to_proc
   end
 end
+
+# Load contact instance and topics for contact form
+module SpreeBase
+  module InstanceMethods
+    protected
+    def load_contact_instance
+      @contact = Contact.new
+    end
+    
+    def load_topics
+      @topics = Topic.all
+    end
+  end
+  
+  class << self
+    def included_with_easy_contact(receiver)
+      included_without_easy_contact(receiver)
+      receiver.send :before_filter, 'load_contact_instance'
+      receiver.send :before_filter, 'load_topics'
+    end
+    
+    alias_method_chain :included, :easy_contact
+  end
+end
+
