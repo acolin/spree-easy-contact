83196cd8d42eea9bc71197a5ec021f94da576b07
Arnoldo Rodriguez Colin <acolin@incaztech.com>
2011-11-04 12:33:53 -0600

    removed spree-easy-contact.js

--- public/javascripts/spree-easy-contact.js
+++
@@ -1,7 +0,0 @@
-//Loads the contact us form via an ajax request
-$(document).ready(function() {
-	//Show spinner if form hasn't loaded yet	
-	$('#contact-us-modal').spin();
-	//Load via ajax the contact form in html
-	$.get("/contacts/new.js")
-});
